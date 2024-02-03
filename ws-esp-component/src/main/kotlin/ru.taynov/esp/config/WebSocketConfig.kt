98bf48e4bc4f45d90d770004250ae9f4056f4dd2
a.taynov <ext.ataynov@tinkoff.ru>
2024-02-03 18:44:37 +0300

    уменьшено время рукопожатия

--- ws-esp-component/src/main/kotlin/ru.taynov/esp/config/WebSocketConfig.kt
+++ ws-esp-component/src/main/kotlin/ru.taynov/esp/config/WebSocketConfig.kt
@@ -2,16 +2,20 @@ package ru.taynov.esp.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
 
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/server")
-            .setAllowedOriginPatterns("*").withSockJS()
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
+            .setTaskScheduler(DefaultManagedTaskScheduler())
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
