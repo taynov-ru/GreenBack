47f8c28c862022e22ecb156b35023b8be9a3c3fe
a.taynov <ext.ataynov@tbank.ru>
2024-09-26 18:23:19 +0300

    проверка на недавнее сообщение перед уведомлением

--- ws-esp-component/src/main/kotlin/ru.taynov/esp/service/EspInteractionService.kt
+++ ws-esp-component/src/main/kotlin/ru.taynov/esp/service/EspInteractionService.kt
@@ -2,8 +2,6 @@ package ru.taynov.esp.service
 
 import mu.KLogger
 import mu.KotlinLogging
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Lazy
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 import ru.taynov.esp.dto.SetParamsResponse
