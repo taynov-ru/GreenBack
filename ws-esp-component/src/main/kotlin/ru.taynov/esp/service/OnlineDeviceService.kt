2d4e2b287940dfa0e8f90325f62df9fd4ca560e0
a.taynov <ext.ataynov@tbank.ru>
2024-09-26 18:23:19 +0300

    проверка на недавнее сообщение перед уведомлением

--- ws-esp-component/src/main/kotlin/ru.taynov/esp/service/OnlineDeviceService.kt
+++ ws-esp-component/src/main/kotlin/ru.taynov/esp/service/OnlineDeviceService.kt
@@ -2,10 +2,7 @@ package ru.taynov.esp.service
 
 import mu.KotlinLogging
 import org.slf4j.Logger
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationEvent
 import org.springframework.context.annotation.Lazy
-import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import ru.taynov.esp.enums.DeviceStatus
@@ -24,7 +21,8 @@ class OnlineDeviceService(
     }
 
     fun setOnline(deviceId: String) {
-        if (onlineDevices.add(deviceId)) {
+        val messageWasRecently = secondsFromLastInteraction(deviceId).let { it != 0L && it < THRESHOLD }
+        if (messageWasRecently.not() && onlineDevices.add(deviceId)) {
             log.info("Connected deviceId: $deviceId")
             espInteractionService.invokeDeviceStatusCallback(deviceId, DeviceStatus.ONLINE)
         }
@@ -32,26 +30,28 @@ class OnlineDeviceService(
 
     @Scheduled(fixedRate = 60000) // каждую минуту
     fun cleanupSessions() {
-        val currentTime = LocalDateTime.now()
         val forCleanup = mutableSetOf<String>()
         onlineDevices.forEach { deviceId ->
-            val creationTime = espInteractionService.getLast(deviceId)?.timestamp?.let {
-                LocalDateTime.ofEpochSecond(it, 0, ZoneOffset.UTC)
-            }
-            if (creationTime != null) {
-                val minutesSinceCreation = ChronoUnit.MINUTES.between(creationTime, currentTime)
-                if (minutesSinceCreation >= THRESHOLDS_MINUTES) {
-                    forCleanup.add(deviceId)
-                    log.info("Disconnected deviceId: $deviceId")
-                    espInteractionService.invokeDeviceStatusCallback(deviceId, DeviceStatus.OFFLINE)
-                }
+
+            val seconds = secondsFromLastInteraction(deviceId)
+            if (seconds >= THRESHOLD) {
+                forCleanup.add(deviceId)
+                log.info("Disconnected deviceId: $deviceId")
+                espInteractionService.invokeDeviceStatusCallback(deviceId, DeviceStatus.OFFLINE)
             }
         }
         onlineDevices.removeAll(forCleanup)
     }
 
+    private fun secondsFromLastInteraction(deviceId: String): Long {
+        val creationTime = espInteractionService.getLast(deviceId)?.timestamp?.let {
+            LocalDateTime.ofEpochSecond(it, 0, ZoneOffset.UTC)
+        }
+        return creationTime?.let { ChronoUnit.SECONDS.between(it, LocalDateTime.now()) } ?: 0
+    }
+
     companion object {
         private val log: Logger = KotlinLogging.logger { }
-        private const val THRESHOLDS_MINUTES = 5
+        private const val THRESHOLD = 300 // 5 min
     }
 }
