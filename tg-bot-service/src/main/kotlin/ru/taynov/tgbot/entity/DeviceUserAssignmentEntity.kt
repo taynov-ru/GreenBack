1e0cfae1740df41baf0d745d16fff10863104721
a.taynov <ext.ataynov@tbank.ru>
2024-10-04 22:33:19 +0300

    fix

--- tg-bot-service/src/main/kotlin/ru/taynov/tgbot/entity/DeviceUserAssignmentEntity.kt
+++ tg-bot-service/src/main/kotlin/ru/taynov/tgbot/entity/DeviceUserAssignmentEntity.kt
@@ -4,10 +4,8 @@ import javax.persistence.ElementCollection
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.Id
-import javax.persistence.Table
 
-@Entity
-@Table(name = "device_user_assignment")
+@Entity(name = "device_user_assignment")
 class DeviceUserAssignmentEntity {
 
     @Id
